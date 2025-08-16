3b17879c11acea479884ef9f09c4688b7887fc0b
Grealishgit <eugyneehunter@gmail.com>
2025-08-17 01:03:43 +0300

    Add initial HTML structure, CSS styles, and JavaScript functionality for city search and interactive elements
    - Created index.html with a basic layout including a search input, city list, and image gallery.
    - Implemented script.js to handle search functionality, filtering city names based on user input.
    - Added CSS styles in style.css for layout, design, and responsiveness of elements.
    - Included interactive pop-up confirmation for search actions.

---
+++ script.js
@@ -0,0 +1,42 @@
+const onSearch = () => {
+    const input = document.querySelector("#search");
+    const filter = input.value.toUpperCase();
+
+    const listItems = document.querySelectorAll("#myList li");
+
+    listItems.forEach(el => {
+        const text = el.textContent.toUpperCase();
+        if (text.includes(filter)) {
+            el.style.display = ""
+            el.style.color = "red"
+        } else {
+            el.style.display = "none"
+        }
+    });
+
+    console.log("Filter value", filter)
+}
+
+
+document.querySelector("#search").addEventListener("input", onSearch)
+
+
+
+
+// const searchInput = document.getElementById("search");
+
+// searchInput.addEventListener("input", () => {
+//     console.log("Input triggered")
+// }
+
+// )
+
+
+const body = document.querySelector("body");
+const element = document.querySelector(".container");
+const light = document.getElementById("light");
+
+element.addEventListener("scroll", () => {
+    const value = 255 - (((light.getBoundingClientRect().top - 8) / 200) * 255);
+    body.style.backgroundColor = `rgb(${value},${value},${value})`
+})
